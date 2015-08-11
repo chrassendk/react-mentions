9f8b8f008f6537dcfb81fdbb631de0c6a9674153
Christian Rasmussen <chrasmus@cisco.com>
2015-08-11 12:33:36 -0700

    oNblUR function

--- src/MentionsInput.jsx
+++ src/MentionsInput.jsx
@@ -411,7 +411,7 @@ module.exports = React.createClass({
       this.setState({
         selectionStart: null,
         selectionEnd: null,
-        changedMention: false
+        changedMention: true
       });
     };
     this._suggestionsMouseDown = false;
